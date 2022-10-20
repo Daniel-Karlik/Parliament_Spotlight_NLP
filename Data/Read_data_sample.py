6bfe0e2f94083c98d64815a02be9311ad1dd397a
Ruzejjur <ruzheinikau@gmail.com>
2022-10-20 21:06:17 +0200

    Added loading datasets from parts
    Datasets ready for data cleaning

--- Data/Read_data_sample.py
+++ Data/Read_data_sample.py
@@ -4,6 +4,8 @@
 import json
 # Packages for constructing path to datasets location
 import os.path
+# Function listdir from os to list files in directory
+from os import listdir
 # Import package regex for deleting part of string matching a pattern
 import re
 # Packages for opening and reading pickle files
@@ -18,7 +20,11 @@ path_to_datasets = "data"
 #                        2. "stenozaznamy_psp.pickle"
 #                        3. "tiskove_konference_vlady.pickle"
 # If you don't want some datasets to be loaded just remove them from the list
-dataset_names = ["vyjadreni_politiku.pickle", "tiskove_konference_vlady.pickle", "stenozaznamy_psp.pickle"]
+dataset_names = ["vyjadreni_politiku", "stenozaznamy_psp", "tiskove_konference_vlady"]
+
+# Choose datasets to load. E.g. if you want to load only vyjadreni_politiku and tiskove_konference_vlady. input [0,2]
+# Note: Only dataset stenozaznamy-psp is dlownladed, others are not of interest right now
+dataset_to_load = [1]
 
 ### ---------------------------------------------------------
 
@@ -27,29 +33,33 @@ dataset_paths = [os.path.join(path_to_datasets, item) for item in dataset_names]
 
 
 def load_dataset(dataset_path):
-    # Opening JSON file with data from datasets
+    # Opening .pickle file with data from datasets
     # Input:
     #       dataset_path - string with relative path to dataset
     # Output:
     #       python dictionary with data
-    with open(dataset_path, 'rb') as openfile: # Setting to read mode, no need for writing in this section
-        # Reading from json file
-        data = pickle.load(openfile)
+    # Getting list of part file of dataset
+    dataset_parts = listdir(dataset_path)
+    # Initializing python dictionary to load parts into
+    data = {"results": []}
+    # Going through files in dataset location
+    for item in dataset_parts:
+        with open(os.path.join(dataset_path, item), 'rb') as openfile: # Setting to read mode, no need for writing in this section
+            # Reading from .pickle file
+            data_part = pickle.load(openfile)
+            # Appending data from part to data
+            data["results"] += data_part["results"]
     return data
 
-counter = 0 # Setting counter for acces of indices of dataset_paths
+#counter = 0 # Setting counter for acces of indices of dataset_paths
 # iterating through each dataset name
-for i in dataset_names:
+for i in dataset_to_load:
     global_Vars = globals() # Getting global variable list to add name of dataset as a variable
-    # Calling load_dataset on current dataset path and assigning it to a local variable
-    # also removing .json from dataset name
-    global_Vars[re.sub('.pickle', '', i)] = load_dataset(dataset_paths[counter])
-    counter += 1 # Incrementing counter by one so that next dataset path in dataset_paths corresponds to correct dataset
-
-# Uncomment for printing data in json nicely
-# stenozaznamy_psp_read = json.dumps(stenozaznamy_psp, indent=4, sort_keys=True, ensure_ascii=False) # Setting json file into readable format
-# # Note: ensure_ascii=False is important so that Czech letters are printed correctly
+    # Calling load_dataset on current dataset path and assigning it to a global variable named after dataset
+    # also removing .pickle from dataset name
+    global_Vars[re.sub('.pickle', '', dataset_names[i])] = load_dataset(dataset_paths[i])
 
 # Examples of accessing info about data entry
 # print(stenozaznamy_psp["results"][1]) # Accessing info about a data entry
 # print(stenozaznamy_psp["results"][1]["text"])  # Example of accessing text in data entry
+s = 1
